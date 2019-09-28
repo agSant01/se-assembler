0cf8541c8e1311a3c72038b2b6adf7acb34bb89c
Gabriel <gabriel.santiago16@upr.edu>
2019-09-27 20:42:33 -0400

    Add project files.

---
+++ Assembler.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assembler.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
