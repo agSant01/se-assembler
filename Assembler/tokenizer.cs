476121ef0a8478d98c91e80ba8a3a03f71220b92
MagnoMestizo <rodrigueznatalandy@gmail.com>
2019-09-27 22:02:46 -0400

    parser, analyzer, tokenizer and op codes added

---
+++ Assembler/tokenizer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Tokenizer
+{
+    private OperationCodes codes;
+	public Tokenizer(string[] line)
+	{
+        
+	}
+
+
+
+    public string[] extract_Tokens()
+    {
+        string[] tokens = { };
+        return tokens;
+    }
+
+    public bool is_Proper_Syntax(string[] line)
+    {
+        return false;
+    }
+}
