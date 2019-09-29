3b17aae8d49a937b8c3f311261ddab1e56298860
Stephanie Garcia <sgribot19@gmail.com>
2019-09-29 17:27:43 -0400

    FIle Manager

---
+++ Assembler/FileManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.IO;
+
+namespace Assembler
+{
+    public class FileManager
+    {
+        public FileManager()
+        {
+            string filePath = @"C:\Users\sgrib\Desktop\example.txt";
+
+            List<string> fileLines = File.ReadAllLines(filePath).ToList();
+
+            foreach (var line in fileLines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+    }
+}
--- Assembler/Program.cs
+++ Assembler/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Assembler
 {
@@ -6,6 +9,14 @@ namespace Assembler
     {
         static void Main(string[] args)
         {
+            //string filePath = @"C:\Users\sgrib\Desktop\example.txt";
+
+            //List<string> fileLines = File.ReadAllLines(filePath).ToList();
+
+            //foreach (var line in fileLines)
+            //{
+            //    Console.WriteLine(line);
+            //}
             Console.WriteLine("Hello World!");
         }
     }
