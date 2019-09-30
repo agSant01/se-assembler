d757e9713859fa0c43ae6ff0e7a2700d29dbaf26
Stephanie Garcia <sgribot19@gmail.com>
2019-09-29 20:01:12 -0400

    File Manager population

--- Assembler/FileManager.cs
+++ Assembler/FileManager.cs
@@ -8,16 +8,41 @@ namespace Assembler
 {
     public class FileManager
     {
-        public FileManager()
+        //public FileManager()
+        //{
+           
+        //    string filePath = @"C:\Users\sgrib\Desktop\example.txt";
+
+        //    List<string> fileLines = File.ReadAllLines(filePath).ToList();
+
+        //    foreach (var line in fileLines)
+        //    {
+        //        Console.WriteLine(line);
+        //    }
+        //}
+
+        public void ToReadFile(string fileName)
         {
-            string filePath = @"C:\Users\sgrib\Desktop\example.txt";
-
-            List<string> fileLines = File.ReadAllLines(filePath).ToList();
-
+            List<string> fileLines = File.ReadAllLines(fileName).ToList();
             foreach (var line in fileLines)
             {
-                Console.WriteLine(line);
+                string filePath = @"C:\Users\sgrib\Desktop\example.txt";
+                File.WriteAllText(filePath, line);
             }
+            
+        }
+
+        public void ToWriteFile(string text)
+        {
+            string filePath = @"C:\Users\sgrib\Desktop\example.txt";
+            File.WriteAllText(filePath, text);
+        }
+
+        public void LoggerFile(string fileName, string message)
+        {
+            string filePath = @"C:\Users\sgrib\Desktop\"+ fileName;
+         
+            File.AppendAllText(filePath, message);
         }
 
     }
