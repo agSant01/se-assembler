9b64cd16a7d717de93c729e6976c30976dcbf638
Stephanie Garcia <sgribot19@gmail.com>
2019-09-30 22:03:01 -0400

    File Manager + Unit Tests completed

--- Assembler.UnitTests/FileManagerTest.cs
+++ Assembler.UnitTests/FileManagerTest.cs
@@ -1,37 +1,63 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using static Assembler.FileManager;
 
 namespace Assembler.UnitTests
 {
-   public class FileManagerTest
+    [TestClass]
+    public class FileManagerTest
     {
-        private FileManager fileManager;
-
         [TestInitialize]
         public void TestInit()
         {
-            fileManager = new FileManager();
         }
 
         [TestMethod]
-        public void FileManagerTester_ReadFile_ShowTextAsOutput()
+        public void FileManagerTester_WriteFile_Success()
         {
-           string nameOfFile = "mock.txt";
-        ToReadFile(nameOfFile);
-           
-            //string filePath = @"C:\Users\sgrib\Desktop\example.txt";
-
-            //List<string> fileLines = File.ReadAllLines(filePath).ToList();
-
-            //foreach (var line in fileLines)
-            //{
-            //    Console.WriteLine(line);
-            //}
+            var textLines = new string[] { "Hello", "World" };
+            var resultPath = FileManager.Instance.ToWriteFile("mock.txt", textLines);
+            Assert.IsTrue(!string.IsNullOrEmpty(resultPath));
         }
 
+        [TestMethod]
+        public void FileManagerTester_WriteFile_Error()
+        {
+            var resultPath = FileManager.Instance.ToWriteFile("mock.txt", null);
+            Assert.IsFalse(!string.IsNullOrEmpty(resultPath));
+        }
+
+        [TestMethod]
+        public void FileManagerTester_ReadFile_Success()
+        {
+            string nameOfFile = "mock.txt";
+            var readLines = FileManager.Instance.ToReadFile(nameOfFile);
+
+            Assert.IsTrue(readLines != null);
+        }
+
+        [TestMethod]
+        public void FileManagerTester_ReadFile_Error()
+        {
+            string nameOfFile = null;
+            var readLines = FileManager.Instance.ToReadFile(nameOfFile);
+
+            Assert.IsFalse(readLines != null);
+        }
+        [TestMethod]
+        public void FileManagerTester_LoggerFile_Success()
+        {
+            string nameOfFile = "log.txt";
+            var logResult = FileManager.Instance.LoggerFile(nameOfFile,"Mock Text");
+
+            Assert.IsTrue(logResult);
+        }
+        [TestMethod]
+        public void FileManagerTester_LoggerFile_Error()
+        {
+            string nameOfFile = null;
+            var logResult = FileManager.Instance.LoggerFile(nameOfFile, "Mock Text");
+
+            Assert.IsFalse(logResult);
+        }
 
     }
 }
--- Assembler/FileManager.cs
+++ Assembler/FileManager.cs
@@ -1,10 +1,4 @@
-﻿/* 
-     * The purpose of this FileManager class is to be able to handle
-     * file actions. Here we have three methods. One for reading from a file, 
-     * one for writing to a file and a last one that acts as the logger file
-     * to keep track of activity.
-     * We are using the File Class (System.IO) */
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
@@ -12,59 +6,111 @@ using System.IO;
 
 namespace Assembler
 {
+    /// <summary>
+    /// This File Manager class has three methods.
+    /// Reading and Writing to a file and a Logger File
+    /// </summary>
     public class FileManager
     {
-        //public FileManager()
-        //{
-
-        //    string filePath = @"C:\Users\sgrib\Desktop\example.txt";
-
-        //    List<string> fileLines = File.ReadAllLines(filePath).ToList();
-
-        //    foreach (var line in fileLines)
-        //    {
-        //        Console.WriteLine(line);
-        //    }
-        //}
-
-        /* 
-         * ToReadFile accepts only one parameter and that is the name of the file to read.
-         * This file name must include its extension.
-         *  It takes the file and makes a list of its lines, then goes through every line 
-         * of the list and rewrites the folder */
-
-        public static void ToReadFile(string fileName)
+        private static FileManager _instance = new FileManager();
+        public static FileManager Instance
         {
-            List<string> fileLines = File.ReadAllLines(fileName).ToList();
-            foreach (var line in fileLines)
-            {       
-                //string filePath = AppDomain.CurrentDomain.BaseDirectory + "\\" + fileName;
-                File.WriteAllText(fileName, line);
+            get
+            {
+                if (_instance == null)
+                    _instance = new FileManager();
+                return _instance;
             }
+        }
+
+        private FileManager()
+        {
+
+        }
+
+        /// <summary>
+        /// Reads all text from a file and returns text seperated in lines.
+        /// </summary>
+        /// <param name="fileName">Name of file to read. Must include file extension</param>
+        /// <returns></returns>
+
+        public string[] ToReadFile(string fileName)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(fileName))
+                    return null;
+
+                if (!fileName.Contains("."))
+                    return null;
+
+                string filePath = AppDomain.CurrentDomain.BaseDirectory + "\\" + fileName;
+                var fileLines = File.ReadAllLines(filePath);
+
+                return fileLines;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
        
+         /// <summary>
+         /// Writes to a file the given text
+         /// </summary>
+         /// <param name="fileName"></param>
+         /// <param name="textLines"></param>
+         /// <returns></returns>
+        public string ToWriteFile(string fileName, string[] textLines)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(fileName))
+                    return null;
+
+                if (!fileName.Contains("."))
+                    return null;
+
+                if (textLines == null)
+                    return null;
+
+                string filePath = AppDomain.CurrentDomain.BaseDirectory + "\\" + fileName;
+                File.WriteAllLines(filePath, textLines);
+                return filePath;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
 
-        /* 
-         * ToWriteFile accepts only one parameter and that is a text you want to 
-         * write to a file.  First it looks for the current directory you are in 
-         * and creates a folder called example to store the info there. */
-
-        public void ToWriteFile(string text)
+        /// <summary>
+        /// Appends the given log message to a file
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="message"></param>
+        public bool LoggerFile(string fileName, string message)
         {
-            string filePath = AppDomain.CurrentDomain.BaseDirectory + "\\example.txt";
-            File.WriteAllText(filePath, text);
-        }
+            try
+            {
+                if (string.IsNullOrEmpty(fileName))
+                    return false;
 
-        /* 
-         * LoggerFile accepts two parameters. One is the name of the file and
-         * the other is the message to be logged. The name of the file must
-         * include its extension. First, it takes the path in which you are
-         * currently in and saves the file there to append the message/logs to it. */
-        public void LoggerFile(string fileName, string message)
-        {
-          
-            string filePath = AppDomain.CurrentDomain.BaseDirectory + "\\" + fileName;
-            File.AppendAllText(filePath, message);
+                if (!fileName.Contains("."))
+                    return false;
+
+                if (message == null)
+                    return false;
+
+                string filePath = AppDomain.CurrentDomain.BaseDirectory + "\\" + fileName;
+                File.AppendAllText(filePath, message);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
     }
