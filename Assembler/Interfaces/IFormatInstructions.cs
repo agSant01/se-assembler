b03b794d3689051d34e0dbf73dfc4c87c1c1709a
Gabriel <gabriel.santiago16@upr.edu>
2019-10-01 21:06:47 -0400

    Implemented InstructionFormat for use with the Assembler.

---
+++ Assembler/Interfaces/IFormatInstructions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assembler.Interfaces
+{
+    public interface IFormatInstructions
+    {
+        string OperatorCode { get; }
+    }
+}
---
+++ Assembler/Parser/InstructionFormats/InstructionFormat1.cs
@@ -0,0 +1,27 @@
+﻿using Assembler.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assembler.Parser.InstructionFormats
+{
+    public class InstructionFormat1 : IFormatInstructions
+    {
+
+        public InstructionFormat1(string opcode, int registerA, int registerB, int registerC )
+        {
+            OperatorCode = opcode;
+            RegisterA = registerA;
+            RegisterB = registerB;
+            RegisterC = registerC;
+        }
+
+        public string OperatorCode { get; }
+
+        public int RegisterA { get; }
+
+        public int RegisterB { get; }
+
+        public int RegisterC { get; }
+    }
+}
