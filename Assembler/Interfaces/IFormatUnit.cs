ab0888f5cb24366a8202d99c4cb99328387bbae3
Gabriel <gabriel.santiago16@upr.edu>
2019-10-03 12:48:21 -0400

    Added documentation for Instructions functionality. Code Refactor. Creation of Utils package to group custom made functionalities.

--- Assembler/Interfaces/IFormatUnit.cs
+++ Assembler/Interfaces/IFormatUnit.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Assembler.Interfaces
+﻿namespace Assembler.Interfaces
 {
+    /// <summary>
+    /// Used to define a type of parameters that is 
+    /// used by the IFormatInstructions.
+    /// </summary>
     public interface IFormatUnit
     {
+        /// <summary>
+        /// Used to determine if the value of the parameter is of valid type.
+        /// </summary>
+        /// <returns>True if the of this param value is valid for this format</returns>
         bool IsValid();
     }
 }
--- Assembler/Parsing/InstructionItems/VariableName.cs
+++ Assembler/Parsing/InstructionItems/VariableName.cs
@@ -1,15 +1,19 @@
 ﻿using Assembler.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Assembler.Parsing.InstructionItems
 {
     public class VariableName : IFormatUnit
     {
-        private bool _isValid;
+        /// <summary>
+        /// True if value is a valid variable name
+        /// </summary>
+        private readonly bool _isValid;
 
+        /// <summary>
+        /// Creates a VariableName instance
+        /// </summary>
+        /// <param name="token">Variable name Token</param>
         public VariableName(Token token)
         {
             Token = token;
@@ -17,10 +21,21 @@ namespace Assembler.Parsing.InstructionItems
             _isValid = new Regex(@"^[a-zA-Z0-9#]*$").IsMatch(token.Value);
         }
 
+        /// <summary>
+        /// Getter for Variable name Token
+        /// </summary>
         public Token Token { get; }
 
+        /// <summary>
+        /// Getter for validity state
+        /// </summary>
+        /// <returns>True if a valid variable name, False otherwise</returns>
         public bool IsValid() => _isValid;
 
+        /// <summary>
+        /// ToString Override
+        /// </summary>
+        /// <returns>String representation of VariableName</returns>
         public override string ToString()
         {
             return Token?.Value;
