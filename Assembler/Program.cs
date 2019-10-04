0c232f9658da93a689e8689e888ac0936c684983
oremorsiul15 <oremorsiul15@hotmail.com>
2019-10-04 12:44:09 -0400

    Console interaction and output file

--- Assembler/Program.cs
+++ Assembler/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Assembler.Assembler;
+using Assembler.Parsing;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,8 +11,83 @@ namespace Assembler
     {
         static void Main(string[] args)
         {
-           
-            Console.WriteLine("Hello World!");
+
+            string val = "";
+            Console.Write("File (with complete Path):");
+
+            while (val.Equals(null) || val.Trim().Equals(""))
+            {
+                val = Console.ReadLine();
+            }
+
+
+            Shell shell = new Shell(val);
+
+            try
+            {
+                shell.outputFile();
+                shell.compiler.Compile();
+                string[] lines = shell.compiler.GetOutput();
+                shell.objectCodeOfFile(lines);
+            }
+            catch (FileNotFoundException) { Console.WriteLine("Couldn't open file...."); }
+
         }
+    }
+
+    public class Shell
+    {
+        //private FileManager fm;
+        private Parser parser;
+        private Lexer lexer;
+        public Compiler compiler;
+        private string path;
+        public Shell(string filename)
+        {
+            path = @filename;
+            try
+            {
+                this.lexer = new Lexer(FileManager.Instance.ToReadFile(path));
+                this.parser = new Parser(this.lexer);
+                this.compiler = new Compiler(parser);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Couldn't open file....");
+            }
+
+        }
+
+        public void outputFile()
+        {
+            if (this.lexer.Equals(null))
+                throw new FileNotFoundException();
+
+
+            Console.WriteLine("Provided File's Contents:\n");
+            while (this.lexer.MoveNext())
+            {
+                Console.WriteLine(this.lexer.CurrrentToken);
+            }
+
+            Console.WriteLine("\n");
+        }
+
+        public bool objectCodeOfFile(string[] lines)
+        {
+            String outputPath = $@"{Path.GetDirectoryName(this.path)}\{Path.GetFileNameWithoutExtension(path)}_HEX_output.txt";
+            Console.WriteLine("output located in : " + outputPath);
+            bool result = FileManager.Instance.ToWriteFile(outputPath, lines);
+
+            return result;
+        }
+
+
+        //public static void Main()
+        //{
+            
+
+        //}
+
     }
 }
