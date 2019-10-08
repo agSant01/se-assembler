57e5faa570b267a78b9500beb6164180d8c546b3
Gabriel <gabriel.santiago16@upr.edu>
2019-10-08 17:56:30 -0400

    Assert added

--- Assembler.UnitTests/ParserTests.cs
+++ Assembler.UnitTests/ParserTests.cs
@@ -35,17 +35,39 @@ namespace Assembler.UnitTests
 
             Assert.IsNotNull(lines, "File Not Found.");
 
+            string[] expected =
+            {
+                "ORIGIN[op: 'org', address: '0', valid: 'True']",
+                "IF3[op: 'JMPADDR', Const/Address: 'start', valid: 'True']",
+                "VariableAssign[op: db, name: valor1, values: [5], valid: 'True']",
+                "VariableAssign[op: db, name: valor2, values: [7], valid: 'True']",
+                "VariableAssign[op: db, name: mayor, values: [0], valid: 'True']",
+                "ConstantAssign[op:const, name: ten, value: 0A, valid: 'True']",
+                "Label[name: start, valid: 'True']",
+                "IF2[op: 'LOAD', Ra: 'R1', Const/Address: 'valor1', valid: 'True']",
+                "IF2[op: 'LOAD', Ra: 'R2', Const/Address: 'valor2', valid: 'True']",
+                "IF1[op: 'GRT', Ra: 'R1', Rb: 'R2', Rc: '', valid: 'True']",
+                "IF3[op: 'JMPADDR', Const/Address: 'R1esMayor', valid: 'True']",
+                "IF2[op: 'STORE', Ra: 'R2', Const/Address: 'mayor', valid: 'True']",
+                "IF3[op: 'JMPADDR', Const/Address: 'fin', valid: 'True']",
+                "Label[name: R1esMayor, valid: 'True']",
+                "IF2[op: 'STORE', Ra: 'R1', Const/Address: 'mayor', valid: 'True']",
+                "IF2[op: 'LOADIM', Ra: 'R3', Const/Address: '#8', valid: 'True']",
+                "Label[name: fin, valid: 'True']",
+                "IF3[op: 'JMPADDR', Const/Address: 'fin', valid: 'True']"
+            };
+
             lexer = new Lexer(lines);
 
             Parser parser = new Parser(lexer);
 
+            int index = 0;
             while (parser.MoveNext())
             {
                 Console.WriteLine(parser.CurrentInstruction);
-                
+                Assert.AreEqual(expected[index], parser.CurrentInstruction.ToString());
+                index++;
             }
-
-
         }
 
         [TestMethod]
@@ -55,13 +77,38 @@ namespace Assembler.UnitTests
 
             Assert.IsNotNull(lines, "File Not Found.");
 
+            string[] expected =
+            {
+                "ORIGIN[op: 'org', address: '0', valid: 'True']",
+                "IF3[op: 'JMPADDR', Const/Address: 'start', valid: 'True']",
+                "VariableAssign[op: db, name: valor1, values: [5], valid: 'True']",
+                "VariableAssign[op: db, name: valor2, values: [7], valid: 'True']",
+                "VariableAssign[op: db, name: mayor, values: [0], valid: 'True']",
+                "InvalidInstruction[op: cst, values: [Token(type: IDENTIFIER, val: 'ten'),Token(type: IDENTIFIER, val: '0A')], valid: 'False']",
+                "Label[name: start, valid: 'True']",
+                "IF2[op: 'LOAD', Ra: 'R1', Const/Address: 'valor1', valid: 'True']",
+                "IF2[op: 'LOAD', Ra: 'R2', Const/Address: 'valor2', valid: 'True']",
+                "IF1[op: 'GRT', Ra: 'R1', Rb: 'R2', Rc: '', valid: 'True']",
+                "InvalidInstruction[op: JMPADR, values: [Token(type: IDENTIFIER, val: 'R1esMayor')], valid: 'False']",
+                "IF2[op: 'STORE', Ra: 'R2', Const/Address: 'mayor', valid: 'True']",
+                "IF3[op: 'JMPADDR', Const/Address: 'fin', valid: 'True']",
+                "Label[name: R1esMayor, valid: 'True']",
+                "InvalidInstruction[op: STOE, values: [Token(type: IDENTIFIER, val: 'mayor'),Token(type: REGISTER, val: 'R1')], valid: 'False']",
+                "IF2[op: 'LOADIM', Ra: 'R3', Const/Address: '#8', valid: 'True']",
+                "Label[name: fin, valid: 'True']",
+                "IF3[op: 'JMPADDR', Const/Address: 'fin', valid: 'True']"
+            };
+
             lexer = new Lexer(lines);
 
             Parser parser = new Parser(lexer);
 
+            int index = 0;
             while (parser.MoveNext())
             {
                 Console.WriteLine(parser.CurrentInstruction);
+                Assert.AreEqual(expected[index], parser.CurrentInstruction.ToString());
+                index++;
             }
         }
     }
