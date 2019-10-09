24d823a918a585b0d518137f6e7248729c988ee9
Alejandro <rodrigueznatalandy@gmail.com>
2019-10-09 15:23:34 -0400

    Added VirtualMemory file with dummy methods

---
+++ Assembler/VirtualMemory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assembler
+{
+    
+    class VirtualMemory
+    {
+        private string[] memory_contents;
+        private string[] memory_addresses;
+
+        public VirtualMemory(string[] contents, string[] addresses)
+        {
+
+        }
+
+        public string[] GetContents(string address)
+        {
+            string[] contents = null;
+            return contents;
+        }
+
+        public string[] GetAddress(string contents)
+        {
+            string[] address = null;
+            return address;
+        }
+
+    }
+}
