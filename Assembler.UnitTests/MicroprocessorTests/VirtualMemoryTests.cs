70e07503da2c965112e2dc3ef017d39c01085e3c
Alejandro <rodrigueznatalandy@gmail.com>
2019-10-10 21:43:59 -0400

    Added UnitTests and some more methods to VirtualMemory class

---
+++ Assembler.UnitTests/MicroprocessorTests/VirtualMemoryTests.cs
@@ -0,0 +1,71 @@
+﻿using Assembler.Microprocessor;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assembler.UnitTests.MicroprocessorTests
+{
+    [TestClass]
+    public class VirtualMemoryTests
+    {
+        //string[] contents;
+        //long[] addresses;
+        VirtualMemory memory;
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Contents of memory greater than address space\n")]
+        public void Virtual_Memory_Init_Uneven_Args_Length_Fail()
+        {
+            long[] addresses = { 0L, 1L, 2L, 3L, 4L };
+            string[] contents = { "a", "b", "c","d","e","f" };
+
+            memory = new VirtualMemory(contents, addresses);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "The address provided is invalid\n")]
+        public void Virtual_Memory_Invalid_Address_Fail()
+        {
+            long[] addresses = { 0L, 1L, 2L };
+            string[] contents = { "a", "b", "c" };
+
+            memory = new VirtualMemory(contents, addresses);
+
+            memory.GetContents(3L);
+        }
+
+
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Contents provided are not in memory \n")]
+        public void Virtual_Memory_Invalid_Contents_Not_In_Memory_Fail()
+        {
+            long[] addresses = { 0L, 1L, 2L };
+            string[] contents = { "a", "b", "c" };
+
+            memory = new VirtualMemory(contents, addresses);
+
+            memory.GetAddress("d");
+        }
+
+
+        [TestMethod]
+        public void Virtual_Memory_To_String_Conversion()
+        {
+            long[] addresses = { 0L, 1L, 2L };
+            string[] contents = { "a", "b", "c" };
+
+            memory = new VirtualMemory(contents, addresses);
+
+            string result = memory.ToString();
+            string desired = "{\n\t[0,a]\n\t[1,b]\n\t[2,c]\n}\n\n";
+
+            Assert.AreEqual(result, desired);
+
+        }
+
+    }
+}
--- Assembler/Microprocessor/VirtualMemory.cs
+++ Assembler/Microprocessor/VirtualMemory.cs
@@ -1,6 +1,90 @@
-﻿namespace Assembler.Microprocessor
+﻿using Assembler.Microprocessor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Assembler.Microprocessor
 {
-    public class VirtualMemory
+    
+    public  class VirtualMemory : Utils.ICustomIterable<string>
     {
+
+        //private string[] memory_contents;
+        //private  long[] memory_addresses;
+
+        private Dictionary<long, string> memory;
+        public VirtualMemory(string[] contents, long[] addresses)
+        {
+            this.memory = new Dictionary<long, string>();
+            LoadMemory(contents, addresses);
+        }
+
+         private void LoadMemory(string[] contents, long[] addresses)
+        {
+            //Fails if len(contents) > len(addresses)
+            if( contents.Length > addresses.Length )
+                throw new Exception("Contents of memory greater than address space\n");
+          
+            for (int i = 0; i < contents.Length; i++)
+                this.memory[addresses[i]] = contents[i];
+        }
+        
+        public string GetContents(long address)
+        {
+            if (memory.ContainsKey(address))
+                return this.memory[address];
+            else
+                throw new Exception("The address provided is invalid\n");
+        }
+
+        public long GetAddress(string contents)
+        {
+            if (memory.ContainsValue(contents))
+            {
+                foreach (KeyValuePair<long, string> item in memory)
+                {
+                    //  Console.WriteLine("Key: {0}, Value: {1}", item.Key, item.Value);
+                    if (item.Value == contents)
+                        return item.Key;
+                }
+            }
+
+            throw new Exception("Contents provided are not in memory \n");
+        }
+
+        public void SetContents(long address, string contents)
+        {
+            if(memory.ContainsKey(address))
+            {
+                this.memory[address] = contents;
+            }
+            else
+            {
+                //we just add both anyway no?
+                this.memory[address] = contents;
+            }
+        }
+
+
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append("{\n");
+            foreach (KeyValuePair<long, string> item in this.memory)
+            {
+                //  Console.WriteLine("Key: {0}, Value: {1}", item.Key, item.Value);
+                builder.Append("\t[" + item.Key + "," + item.Value + "]\n");
+            }
+            builder.Append("}\n\n");
+
+            return builder.ToString();
+
+        }
+
+
+
     }
 }
