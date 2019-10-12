567c804964cd2a3a1d561616c32e3936f389698c
Gabriel <gabriel.santiago16@upr.edu>
2019-10-12 15:21:29 -0400

    Added UnitsConverter and its respective tests.

---
+++ Assembler/Utils/UnitsConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assembler.Utils
+{
+    public static class UnitsConverter
+    {
+        public static string DecimalToHex(int decimalNumber)
+        {
+            return String.Format("0x{0:X}", decimalNumber);
+        }
+
+        public static int HexToDecimal(string hexNumber)
+        {
+            return Convert.ToInt32(Convert.ToInt64(hexNumber, 16));
+        }
+
+        public static string HexToBinary(string hexNumber)
+        {
+            return Convert.ToString(Convert.ToInt32(hexNumber, 16), 2);
+        }
+    }
+}
