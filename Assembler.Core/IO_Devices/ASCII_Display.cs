4cf5388fb36eb09e35b2981271aec9f66d09b91d
Alejandro <rodrigueznatalandy@gmail.com>
2019-11-01 02:03:45 -0400

    Added ASCII Character Display dummy  Logic and empty UnitTest

---
+++ Assembler.Core/IO_Devices/ASCII_Display.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assembler.Core.IO_Devices
+{
+    public class ASCII_Display
+    {
+        private readonly string[] ascii_chars = {"A","B","C","D","E","F","G","H" };
+        private readonly byte DEFAULT = 0;
+        private byte[] characters;//these need to be mapped contigously on VirtualMemory
+        public ASCII_Display()
+        {
+            this.characters = new byte[8];
+        }
+
+        public void addCharacter(int idx, byte b)
+        {
+            if (!IsValidIndex(idx))
+                return;
+
+            this.characters[idx] = b;
+        }
+
+        public void RemoveCharacter(int idx)
+        {
+            if (!IsValidIndex(idx))
+                return;
+
+            this.characters[idx] = DEFAULT;
+        }
+
+        private bool IsValidIndex(int idx)
+        {
+            if (idx > this.characters.Len | idx == this.characters.Len | idx < 0)
+                return false;
+            return true;
+        }
+
+        public string GetChar(int idx)
+        {
+            if (!IsValidIndex(idx))
+                return "";
+
+            byte res = GetByte(idx);
+            if ( res == DEFAULT)
+            {
+                return "";
+            }
+
+            else
+            {
+                return res;
+            }
+        }
+
+
+        public byte GetByte(int idx)
+        {
+            if (!IsValidIndex(idx))
+                return DEFAULT;
+
+            return this.characters[idx];
+        }
+    }
+}
---
+++ Assembler.UnitTests/IO_DevicesTests/ASCII_DisplayTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+
+namespace Assembler.UnitTests.IO_DevicesTests
+{
+    [TestClass]
+    class ASCII_DisplayTests
+    {
+
+        [TestMethod]
+        public void TestValidIndexes()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestInvalidIndexes()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestDefaultValueValidIndexes()
+        {
+
+        }
+    }
+}
