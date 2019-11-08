a7f69c33a3a1a9331ecdeb41fc551a4fd84ec151
oremorsiul15 <oremorsiul15@hotmail.com>
2019-11-08 18:38:34 -0400

    IO semaforo implemented

---
+++ Assembler.Core/Microprocessor/IO/IODevices/IOBinSemaforo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assembler.Core.Microprocessor.IO.IODevices
+{
+    class IOBinSemaforo: IIODevice
+    {
+        public short IOPort { get; }
+
+        public short IOPortLength => 1;
+
+        public bool HasData => _bitContent == null;
+
+        public string DeviceName => "IO Bin Semaforo";
+
+        private char[] _bitContent;
+
+        public IOBinSemaforo(short ioPort)
+        {
+            IOPort = ioPort;
+        }
+
+
+        public bool WriteInPort(int port, string contentInHex)
+        {
+            string binVal = Utils.UnitConverter.HexToBinary(contentInHex);
+            _bitContent = binVal.ToCharArray();
+            return true;
+        }
+
+        public bool Reset()
+        {
+            _bitContent = null;
+            return true;
+        }
+
+        public string ReadFromPort(int port)
+        {
+            throw new InvalidOperationException("Invalid call on a write only device");
+        }
+    }
+}
---
+++ Simulator UI/IOBinSemaforo.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Assembler.Core.Microprocessor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Simulator_UI
+{
+
+    /// <summary>
+    /// Interaction logic for IOBinSemaforo.xaml
+    /// </summary>
+    public partial class IOBinSemaforo : Window
+    {
+        private readonly IOManager _ioManager;
+        //file:///C:/Users/oremo/Downloads/Sprint%203%20DispositivosIO.pdf
+        public IOBinSemaforo(IOManager ioManager)
+        {
+            InitializeComponent();
+            _ioManager = ioManager;
+        }
+    }
+}
