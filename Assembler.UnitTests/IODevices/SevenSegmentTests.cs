ac84f082367fbd49447558e323302b6c85789ca3
Stephanie Garcia <sgribot19@gmail.com>
2019-11-13 15:36:23 -0400

    steph test

--- Assembler.UnitTests/IODevices/SevenSegmentTests.cs
+++ Assembler.UnitTests/IODevices/SevenSegmentTests.cs
@@ -25,5 +25,50 @@ namespace Assembler.UnitTests.IODevices
 
             Console.WriteLine(display);
         }
+
+        [TestMethod]
+        public void SevenSegmentTestsTests_WriteFromMicro_Success()
+        {
+            VirtualMemory vm = new VirtualMemory(new string[] {
+                "a806",
+                "ff07",
+                "0000",
+                "0102",
+                "0203",
+                "c940",
+                "a812",
+                "1a04",
+                "a816",
+                "1904",
+                "0b08",
+                "a816"
+            });
+
+            IOManager manager = new IOManager(100);
+
+            MicroSimulator micro = new MicroSimulator(vm, manager);
+
+            Console.WriteLine("Initial State:");
+            Console.Write(vm);
+            Console.WriteLine(manager);
+            Console.WriteLine(micro);
+
+            IOSevenSegmentDisplay segment = new IOSevenSegmentDisplay(9);
+
+            manager.AddIODevice(9, segment);
+
+            Console.WriteLine("\nAfter adding IO Hex Keyboard:");
+            Console.WriteLine(manager);
+
+            for (int i = 0; i < 7; i++)
+                micro.NextInstruction();
+
+            foreach (char c in segment.Data)
+                Assert.AreEqual("11110110", segment.Data);
+
+            Console.WriteLine(segment);
+
+
+        }
     }
 }
